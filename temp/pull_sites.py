5324f19452a5763dc02f582860256bd5d9e5f89c
cjkang <cjkang@gmail.com>
2019-12-12 16:18:31 -0600

    Update pull_sites.py

--- temp/pull_sites.py
+++ temp/pull_sites.py
@@ -2,11 +2,16 @@ import hail as hl
 hl.init("default_reference=GRCh38")
 
 # pull EOCAD sites
+mt = hl.read_matrix_table("gs://wustl-gatk4-call-set-20190614/postvqsr/all_chr.ht")
 
+mt = mt.filter_rows(hl.is_snp(mt.alleles))
+
+mt = mt.filter_cols(hl.s == )
+hl.export_vcf(mt,"gs://cj-hail-practice/eocad.vcf.bgz")
 
 # pull Fin sites
 for x in chroms:
-  mt = hl.read_matrix_table("wustl-gatk4-final-call-set-20190319/merged_mt"+x)
+  mt = hl.read_matrix_table("gs://wustl-gatk4-final-call-set-20190319/merged_mt"+x)
   # filter snp
   mt = mt.filter_rows(hl.is_snp(mt.alleles))
   # filter first sample
